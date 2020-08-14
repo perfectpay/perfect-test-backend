c6ed07d83c48e46dd3cfaa654a6ec940789ad8b4
Leonardo Zanette <leonardoz@gmail.com>
2020-08-14 16:18:50 -0300

    Feature: Telas para teste com padrões Perfect

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+
+/*
+Telas para ver o funcionamento sem dados
+*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('/sales', function () {
+    return view('crud_sales');
+});
+Route::get('/products', function () {
+    return view('crud_products');
 });
