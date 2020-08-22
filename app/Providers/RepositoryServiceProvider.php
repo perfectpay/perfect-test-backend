15fe565782f4e57b9037edbfee22dd95323a0798
Gabriel Costa <gubsocosta@gmail.com>
2020-08-22 19:59:57 -0300

    create repository service provider and product repository

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Eloquent\ProductRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ app/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+/**
+ * interface ProductRepository
+ *
+ * @package App\Repositories\Eloquent
+ */
+class ProductRepository
+{
+}
