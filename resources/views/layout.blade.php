09560af8f958fa558998736c004062938ced612b
Ricardo Lima <ricardo@vitrynedigital.com.br>
2021-03-31 16:24:04 -0300

    update files blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <title>Perfect Pay - Teste - @yield('title')</title>
     <link href="{{ asset('/images/brand/favicon.png') }}" rel="icon" type="image/png"/>
     <link rel='stylesheet' href="{{ url('/css/app.css') }}">
+    <link rel='stylesheet' href="{{ url('/css/custom.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
     <style>
         .wrapper #wrapperContent, .wrapper #wrapperContent.closed {
@@ -23,6 +24,15 @@
 </div>
 <script src="{{ url('/js/app.js') }}"></script>
 <script src="https://kit.fontawesome.com/d712964458.js" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script src="{{ asset('js/jquery.price_format.min.js')}}"></script>
+<script src="{{ asset('js/jquery.mask.js')}}"></script>
+<script src="{{ asset('js/funcoes.comum.js')}}"></script>
+<script type="text/javascript">
+    $.ajaxSetup({
+         headers : {'X-CSRF-TOKEN': "{{ csrf_token()}}"}
+    });
+</script>
 @yield('script')
 </body>
 </html>
