d97ba2dac6e65d1525f1051a696563c7411effd1
davissoncsm <davisson.charles@gmail.com>
2021-05-12 14:47:51 -0300

    Feat: change request to API

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::resource('/client', ClientController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/product', ProductController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy']);
+
+Route::resource('/sale', SaleController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy']);
+
+
