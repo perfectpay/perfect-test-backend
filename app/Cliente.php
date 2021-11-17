5190048bd0c1002fce8ee12e4722e88b961c0a67
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-17 17:30:28 -0300

    feat: add relationships in model Cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -12,4 +12,19 @@ class Cliente extends Model
         'email',
         'cpf',
     ];
+
+    /**
+     * Retorna todas as vendas do cliente.
+     */
+    public function vendasCliente(){
+        return $this->hasMany(Venda::class);
+    }
+
+    /**
+     * Retorna todos os produtos comprados pelo cliente.
+     */
+    public function produtosCliente()
+    {
+        return $this->hasManyThrough(Produto::class, Venda::class);
+    }
 }
