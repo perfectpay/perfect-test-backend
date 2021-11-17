46984499c14d6f4fb60075f87d644b2c29b0bf02
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-17 19:45:44 -0300

    fix: ClienteRequest cpf property

--- app/Http/Requests/ClienteRequest.php
+++ app/Http/Requests/ClienteRequest.php
@@ -37,7 +37,7 @@ class ClienteRequest extends FormRequest
         return [
             'name'=>'required|min:3|max:100',
             'email'=> (!empty($this->request->all()['id']) ? 'required|email:rfc,dns|max:100|unique:cliente,email,' . $this->request->all()['id'] : 'required|email:rfc,dns|max:100|unique:cliente,email'),
-            'cpf'=> (!empty($this->request->all()['id']) ? 'required|unique:cliente,email,' . $this->request->all()['id'] : 'required|unique:cliente,email'),
+            'cpf'=> (!empty($this->request->all()['id']) ? 'required|unique:cliente,cpf,' . $this->request->all()['id'] : 'required|unique:cliente,cpf'),
         ];
     }
 }
