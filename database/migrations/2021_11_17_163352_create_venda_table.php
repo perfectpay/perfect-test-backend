da704f541a61cb8ffa7710b448fd0b809b929c3e
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-17 17:06:09 -0300

    feat: create venda migration

---
+++ database/migrations/2021_11_17_163352_create_venda_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('data');
+            $table->smallInteger('quantidade');
+            $table->tinyInteger('desconto')->nullable();
+            $table->string('status', 20);
+            $table->foreignId('cliente_id')->references('id')->on('cliente')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venda');
+    }
+}
