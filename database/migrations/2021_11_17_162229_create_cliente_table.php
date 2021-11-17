a115c432fe02bbe06400331de033401eee6f968d
diegosantosouza <diegosantos.s@hotmail.com>
2021-11-17 16:32:55 -0300

    feat: create cliente migration

---
+++ database/migrations/2021_11_17_162229_create_cliente_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('cpf')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
