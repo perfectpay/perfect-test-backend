cddcf73eade7a7492ff51c2bc7484377ff864bde
Wilson O Junior <wjunior013@gmail.com>
2022-06-29 09:52:54 -0300

    blades

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<html lang='pt-br'>
+<head>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <title>Perfect Pay - Teste - @yield('title')</title>
+    <link href="{{ asset('/images/brand/favicon.png') }}" rel="icon" type="image/png"/>
+    <link rel='stylesheet' href="{{ url('/css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <style>
+        .wrapper #wrapperContent, .wrapper #wrapperContent.closed {
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+<!-- NAVBAR TOP -->
+@include('layout_header')
+<div class='wrapper'>
+    <div id='wrapperContent' class='content container-fluid'>
+        <div id='main'>
+            @yield('content')
+        </div>
+    </div>
+</div>
+<script src="{{ url('/js/app.js') }}"></script>
+<script src="https://kit.fontawesome.com/d712964458.js" crossorigin="anonymous"></script>
+@yield('script')
+</body>
+</html>
